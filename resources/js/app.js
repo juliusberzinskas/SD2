8317af9e60b7786277cb78143315829a4ed4535e
Julius <berzinskas.julius@gmail.com>
2026-01-03 14:03:54 +0200

    sukurta webpack, package.json

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-import './bootstrap';
+import 'bootstrap';
+import Swal from 'sweetalert2';
+
+window.Swal = Swal;
+
+// delete confirm helper
+window.confirmDelete = (formId) => {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: 'This action cannot be undone.',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, delete',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      document.getElementById(formId).submit();
+    }
+  });
+};
